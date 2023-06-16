86ba436d4d44f9d8a3ebc93404099f35a58d850e
Enoch ayomide <Enocholuwawalehimin@gmail.com>
2023-06-16 01:32:59 -0700

    cypress automation setup

---
+++ cypress/integration/examples/Testfile2.js
@@ -0,0 +1,7 @@
+/// <reference  types='cypress'/>
+
+describe('Navigate to the web page', function() {
+  it('Navigate to landing page', function() {
+    cy.visit('https://staging.qwid.io')
+  })
+})
